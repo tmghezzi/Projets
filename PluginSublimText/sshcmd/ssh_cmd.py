19b4ecb90fde1d5c5e886783d8be495c5fcb6851
tmghezzi <mghezzi.tahar@hotmail.fr>
2020-08-14 11:03:12 +0200

    Update ssh_cmd.py

--- PluginSublimText/sshcmd/ssh_cmd.py
+++ PluginSublimText/sshcmd/ssh_cmd.py
@@ -1,3 +1,6 @@
+#Sublimetext local/distant command 
+#require pscp 
+
 import sublime, sublime_plugin
 import os
 from subprocess import PIPE, Popen
@@ -15,6 +18,7 @@ class SshCmdCommand(sublime_plugin.TextCommand):
         fileName = os.path.basename(self.view.file_name())
         pathName = os.path.dirname(self.view.file_name())
         os.chdir(pathName)
-        cmdstr = "pscp -pw sd123456 -P 2222 "+fileName+" galois@127.0.0.1:/usr/home/galois"
+        #replace your_pwd by your password and galois@127.0.0.1:/usr/home/galois by your local/distant machine
+        cmdstr = "pscp -pw your_pwd -P 2222 "+fileName+" galois@127.0.0.1:/usr/home/galois"
         print cmdstr
         cmdline(cmdstr)
